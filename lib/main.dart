904284910f2057cd7218b92bf53c2050ff4174f4
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-09-17 10:56:42 -0300

    fix: change to run in correct platform

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,6 @@ import 'package:contacts_app/ios/ios.app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  //Platform.isIOS ? runApp(IOSApp()) : runApp(AndroidApp());
-  runApp(IOSApp());
+  Platform.isIOS ? runApp(IOSApp()) : runApp(AndroidApp());
+  //runApp(IOSApp());
 }
